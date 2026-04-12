4f485acac9a651fb4c3e49c7bc9c3fc3dd02f58c
Aakash555Verma <aakash555verma@gmail.com>
2026-04-12 15:45:20 +0530

    First Commit

---
+++ src/main/java/org/springbootcallingthirdpartyapi/SpringBootCallingThirdPartyApiApplication.java
@@ -0,0 +1,13 @@
+package org.springbootcallingthirdpartyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCallingThirdPartyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCallingThirdPartyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/springbootcallingthirdpartyapi/SpringBootCallingThirdPartyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springbootcallingthirdpartyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCallingThirdPartyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
